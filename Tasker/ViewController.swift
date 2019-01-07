d61157845c6252d4bd2bc45720200faeed78ad10
Denis Filipas <maskzp@gmail.com>
2019-01-07 12:53:26 +0200

    Initial Commit

---
+++ Tasker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasker
+//
+//  Created by Denis Filipas on 1/7/19.
+//  Copyright © 2019 Denis Filipas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
